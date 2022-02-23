1d4cd9dbccbbec7b8dec075f78decfa33471071d
offiahchiaz <offiahchiaz@gmail.com>
2022-02-23 16:59:58 +0100

    Displaying content only when auth is checked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -6,16 +7,20 @@ import Signup from "./pages/signup/Signup";
 
 
 function App() {
+
+  const { authIsReady } = useAuthContext()
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <Switch>
-          <Route exact path="/"><Home /> </Route>
-          <Route path="/login"><Login /> </Route>
-          <Route path="/signup"><Signup /> </Route>
-        </Switch>
-      </BrowserRouter>
+      { authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/"><Home /> </Route>
+            <Route path="/login"><Login /> </Route>
+            <Route path="/signup"><Signup /> </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
