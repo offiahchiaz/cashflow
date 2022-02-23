a0ae48311d52fa5c9f833d5019ebfd8c18efee88
offiahchiaz <offiahchiaz@gmail.com>
2022-02-23 22:28:13 +0100

    Added TransactionForm component for handling transaction creation

---
+++ src/pages/home/TransactionForm.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+
+export default function TransactionForm() {
+
+  const [name, setName] = useState('')
+  const [amount, setAmout] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <h3>Add a Transaction</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Transaction name:</span>
+          <input 
+            type="text" 
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Amount ($):</span>
+          <input 
+            type="number" 
+            required
+            onChange={(e) => setAmout(e.target.value)}
+            value={amount}
+          />
+        </label>
+        <button>Add Transaction</button>
+      </form>
+    </>
+  )
+}
