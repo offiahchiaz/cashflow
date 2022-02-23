5b65242117f104de744601768db883dc58d49db9
offiahchiaz <offiahchiaz@gmail.com>
2022-02-23 22:29:01 +0100

    Home updated with Transction creation

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,16 @@
+import TransactionForm from './TransactionForm'
 import styles from './Home.module.css'
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <p>Transaction list</p>
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
   )
 }
