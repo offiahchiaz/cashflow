ec5c137518c6e32bd82dad62c1466d973f07a48c
offiahchiaz <offiahchiaz@gmail.com>
2022-02-24 23:24:43 +0100

    Added useFirestore hook for handling all firestore database interactions

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,46 @@
+import { useEffect, useReducer, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: null
+}
+
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    case '':
+      return ''
+  
+  
+    default:
+      return state
+  }
+}
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState)
+  const [isCancelled, setIsCancelled] = useState(false)
+
+  // collection ref
+  const ref = projectFirestore.collection(collection)
+
+  // add a document
+  const addDocument = (doc) => {
+
+  }
+
+  // delete a document
+  const deleteDocument = (id) => {
+
+  }
+
+  useEffect(() => {
+    // returns a clean up function
+    return () => setIsCancelled(true)
+  }, [])
+
+  return { addDocument, deleteDocument, response }
+}
