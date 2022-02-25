befe2beff3a2ce7be1586623f5ceaa312ff07f27
offiahchiaz <offiahchiaz@gmail.com>
2022-02-25 02:41:24 +0100

    Added timestamp for createdAt property

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer, useState } from "react"
-import { projectFirestore } from "../firebase/config"
+import { projectFirestore, timestamp } from "../firebase/config"
 
 
 let initialState = {
@@ -42,7 +42,8 @@ export const useFirestore = (collection) => {
     dispatch({ type: 'IS_PENDING' })
 
     try {
-      const addedDocument = await ref.add(doc)
+      const createdAt = timestamp.fromDate(new Date())
+      const addedDocument = await ref.add({ ...doc, createdAt })
       dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
     } 
     catch (err) {
